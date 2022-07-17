a464682226caffff1f68c8a31de753477cdea250
ozanpolat48 <mrozanpolat@gmail.com>
2022-07-18 00:26:52 +0300

    work 3 is done, about to be pushed, to Github after, commit.

--- src/Git.java
+++ src/Git.java
@@ -1,16 +1,12 @@
 public class Git {
 
+    // Doing some work here (you can type anything you want)
 
-    public static void main(String[] args) {
+    // Doing awesome work 2 !
 
-
-        System.out.println("hello world");
-
-        System.out.println("hello sky");
-
-        // Doing awesome work 2 !
+        // Doing awesome work 3 !
         // Awesome work 3 is done and ready to be committed
         // and ready to be pushed to remote repository
 
     }
-}
+
