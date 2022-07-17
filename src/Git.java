9be6b894f458c4141f01049f6773f13ab5324a9d
ozanpolat48 <mrozanpolat@gmail.com>
2022-07-18 00:20:59 +0300

    work 3 is done, about to be pushed, to Github after, commit

--- src/Git.java
+++ src/Git.java
@@ -9,7 +9,8 @@ public class Git {
         System.out.println("hello sky");
 
         // Doing awesome work 2 !
-
+        // Awesome work 3 is done and ready to be committed
+        // and ready to be pushed to remote repository
 
     }
 }
